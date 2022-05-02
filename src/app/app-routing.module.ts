ef895ec579c9e3addb1bf88fb0f5fd0c430a3b92
arnaldorcm <arnaldo1ooo95@gmail.com>
2022-05-02 10:47:28 -0400

    Revert "Se añadió el routing del Modulo Cursos"
    This reverts commit 263004d00b7ec04244d90e16e60b270ce7440eeb.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '',  pathMatch: 'full',redirectTo: 'cursos' },  //Cuando path es vacio, va a ser la ruta por defecto
-  { path: 'cursos', loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule) }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
