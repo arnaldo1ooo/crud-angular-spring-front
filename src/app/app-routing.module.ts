724f71ba0a728e04a4e6e09275d01e2cd9c146fe
arnaldorcm <arnaldo1ooo95@gmail.com>
2022-05-02 10:51:26 -0400

    Se añadió el routing del Modulo Cursos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  pathMatch: 'full',redirectTo: 'cursos' },  //Cuando path es vacio, va a ser la ruta por defecto
+  { path: 'cursos', loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
