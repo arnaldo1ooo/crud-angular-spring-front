3463e66858f4465579da624c608b05105ad2d042
arnaldorcm <arnaldo1ooo95@gmail.com>
2022-07-18 14:19:34 -0400

    Se agregó select Categoria a form de Cursos

--- src/app/compartido/material-modulos/material-modulos.module.ts
+++ src/app/compartido/material-modulos/material-modulos.module.ts
@@ -8,6 +8,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { MatInputModule } from '@angular/material/input';
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule
   ],
 
 })
